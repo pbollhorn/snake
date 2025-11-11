e1ba9c5ae7b4dd51890fa4717e9eb9ce1e316c35
Peter Bollhorn <pbollhorn@gmail.com>
2025-11-11 16:19:07 +0100

    copied files over from game-of-life

---
+++ controller.js
@@ -0,0 +1,34 @@
+import * as view from "./view.js";
+import * as model from "./model.js";
+
+export function resetBoard(rows, cols) {
+  model.resetBoard(rows, cols);
+}
+
+export function setCell(row, col) {
+  model.setCell(row, col);
+  const grid = model.getBoard();
+  view.displayBoard(grid);
+}
+
+export function addRandomCells() {
+  model.addRandomCells();
+  view.displayBoard(model.getBoard());
+}
+
+export async function runGame() {
+  while (true) {
+    model.nextGeneration();
+    view.displayBoard(model.getBoard());
+    view.displayGenerationNumber(model.getGenerationNumber());
+    await sleep(500);
+  }
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+// Startup code which is only run once - on page load
+view.registerEventHandlers();
+view.clickedResetBoardButton();
---
+++ view.js
@@ -0,0 +1,87 @@
+import * as controller from "./controller.js";
+
+const board = document.getElementById("board");
+const generationCounter = document.getElementById("generationCounter");
+const resetBoardButton = document.getElementById("resetBoardButton");
+const addRandomCellsButton = document.getElementById("addRandomCellsButton");
+const runGameButton = document.getElementById("runGameButton");
+const rowsInput = document.getElementById("rowsInput");
+const colsInput = document.getElementById("colsInput");
+
+export function registerEventHandlers() {
+  resetBoardButton.addEventListener("click", clickedResetBoardButton);
+  addRandomCellsButton.addEventListener("click", clickedAddRandomCellsButton);
+  runGameButton.addEventListener("click", clickedRunGameButton);
+  board.addEventListener("click", clickedBoard);
+}
+
+export function clickedResetBoardButton() {
+  const rows = rowsInput.value;
+  const cols = colsInput.value;
+
+  // This removes all children from the board
+  board.innerHTML = "";
+
+  // Sets the CSS property which controls number of columns
+  board.style.gridTemplateColumns = `repeat(${cols}, max-content)`;
+
+  // Loop over rows and cols and create child elements for the board (the cells)
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+      board.appendChild(createCellElement(row, col));
+    }
+  }
+
+  controller.resetBoard(rows, cols);
+}
+
+function clickedAddRandomCellsButton() {
+  controller.addRandomCells();
+}
+
+function clickedRunGameButton() {
+  rowsInput.disabled = true;
+  colsInput.disabled = true;
+  resetBoardButton.disabled = true;
+  addRandomCellsButton.disabled = true;
+  controller.runGame();
+}
+
+function createCellElement(row, col) {
+  let cell = document.createElement("div");
+  cell.className = "cell empty";
+  cell.setAttribute("data-row", row);
+  cell.setAttribute("data-col", col);
+  return cell;
+}
+
+function clickedBoard(event) {
+  const target = event.target;
+  if (target.classList.contains("cell")) {
+    const row = parseInt(target.dataset.row);
+    const col = parseInt(target.dataset.col);
+    controller.setCell(row, col);
+  }
+}
+
+export function displayBoard(grid) {
+  // Loop over all children of the board (which are the cells)
+  for (const cell of board.children) {
+    const row = parseInt(cell.dataset.row);
+    const col = parseInt(cell.dataset.col);
+
+    // Set CSS classes for cell depending on true/false value in grid
+    switch (grid.get({ row, col })) {
+      case true:
+        cell.className = "cell filled";
+        break;
+      case false:
+        cell.className = "cell empty";
+        break;
+    }
+  }
+}
+
+export function displayGenerationNumber(generationNumber) {
+  generationCounter.innerText = generationNumber;
+}
